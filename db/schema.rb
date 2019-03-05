19987fb04861a0756dae03643d75dcbcfbaa6760
n.wada <jones3429@outlook.jp>
2019-03-05 16:38:24 +0900

    add Comment model and create association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_064618) do
+ActiveRecord::Schema.define(version: 2019_03_05_072909) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
