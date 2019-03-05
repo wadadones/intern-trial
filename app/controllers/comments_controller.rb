0008da7bf3a85084e2a504afe3aaaada3399a2c9
n.wada <jones3429@outlook.jp>
2019-03-06 01:11:15 +0900

    記事、コメントの編集、削除、追加機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,67 @@
 class CommentsController < ApplicationController
-	
+	before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+	def new
+		@comment = Comment.new
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+  end
+
+	def create
+	 	p comment_params
+	 	p "ぽぽぽぽ"
+    @comment = Comment.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+      	#binding.pry
+      	p @comment
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+    	p comment_params
+      if @comment.update(comment_params)
+      	
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+  	article_id = @comment.article.id
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to article_url(article_id), notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :article_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
   resources :comments
+  get "/comments/:article/new", to: "comments#new"
+  get "/comments/:article/:id/edit", to:"comments#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
