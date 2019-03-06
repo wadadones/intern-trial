0bb707019a8b3b270aacc0791cd5f5bc96305f9b
n.wada <jones3429@outlook.jp>
2019-03-07 00:16:12 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :comments
+  resources :articles 
+  resources :comments, :except => [:new, :edit]
   get "/comments/:article/new", to: "comments#new"
   get "/comments/:article/:id/edit", to:"comments#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
