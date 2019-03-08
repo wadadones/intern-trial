6a245fe80adb6320734247a811ef27a273c87f16
n.wada <jones3429@outlook.jp>
2019-03-08 17:51:35 +0900

    pathヘルパー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :articles 
   resources :comments, :except => [:new, :edit]
-  get "/comments/:article/new", to: "comments#new"
-  get "/comments/:article/:id/edit", to:"comments#edit"
+  get "/comments/:article/new", to: "comments#new", as: "new_comment"
+  get "/comments/:article/:id/edit", to:"comments#edit", as: "edit_comment"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
