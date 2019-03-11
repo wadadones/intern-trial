eb8b8325926de43c3b377f0e6c75890264dd3ab1
n.wada <jones3429@outlook.jp>
2019-03-11 09:12:30 +0900

    migrationコメント追加

--- db/migrate/20190305064618_create_articles.rb
+++ db/migrate/20190305064618_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.string :content
+    create_table :articles, comment: '記事' do |t|
+      t.string :title, comment: 'タイトル'
+      t.string :content, comment: '内容'
       t.timestamps
     end
   end
--- db/migrate/20190305065437_create_comments.rb
+++ db/migrate/20190305065437_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
-      t.string :content
+    create_table :comments, comment: 'コメント' do |t|
+      t.string :content, comment: '内容'
       t.timestamps
     end
   end
